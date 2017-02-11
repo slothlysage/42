6472b727e4618bbd521295f2ebfa1ec4f55a1efb
Sean JONES <sjones@e1z3r5p19.42.us.org>
2017-02-10 22:01:30 -0800

    changes

--- hercules/hide_ssh.sh
+++ hercules/hide_ssh.sh
@@ -6,15 +6,15 @@
 #    By: sjones <marvin@42.fr>                      +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2017/02/10 20:39:10 by sjones            #+#    #+#              #
-#    Updated: 2017/02/10 21:59:29 by sjones           ###   ########.fr        #
+#    Updated: 2017/02/10 22:01:24 by sjones           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 if $(id -u) != 0; then exit 1
 fi
 sed -i 's/# deb cdrom/deb cdrom/' /etc/apt/sources.list
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get install openssh-server
+apt-get update
+apt-get upgrade
+apt-get install openssh-server
 sed -i 's/port 22/port 65521/' /etc/ssh/sshd_config
 service ssh restart
