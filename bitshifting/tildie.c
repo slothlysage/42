572649552bb1caf75de9baeaff7ac604ef18a8c9
= <=>
2017-10-15 01:48:06 -0700

    added error handling to tildie

--- bitshifting/tildie.c
+++ bitshifting/tildie.c
@@ -3,9 +3,12 @@
 #define LENGTH 4
 
 char *tildie(char *bits) {
-	for (int i = 0; i < LENGTH; i++)
-		bits[i] = bits[i] == '0' ? '1' : '0';
-	return bits;
+	if (bits[0] == '~') {
+		for (int i = 1; i - 1 < LENGTH; i++)
+			bits[i] = bits[i] == '0' ? '1' : '0';
+		return bits + 1;
+	}
+	return (bits);
 }
 
 int main(int ac, char **av) {
